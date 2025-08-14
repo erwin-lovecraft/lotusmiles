150512949778400b7c0677e134025b8dee986f1c
the-witcher-knight <dhuuloc8818@gmail.com>
2025-08-14 20:58:35 +0700

    Improve home page

--- user/src/components/appbar.tsx
+++ user/src/components/appbar.tsx
@@ -1,14 +1,13 @@
 import { Button } from "@/components/ui/button.tsx";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { Bell, LogOut, User } from "lucide-react";
+import { Bell, LogOut, Plane, User } from "lucide-react";
 import { useAuth0 } from "@auth0/auth0-react";
-import logo from "@/assets/logo.svg";
 import {
   DropdownMenu,
   DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator, DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu.tsx";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar.tsx";
 
 export default function AppBar() {
   const {user, isAuthenticated, loginWithRedirect, logout} = useAuth0();
@@ -29,10 +28,9 @@ export default function AppBar() {
           <div className="relative">
             <div
               className="w-8 h-8 sm:w-10 sm:h-10 bg-gradient-to-br from-purple-600 to-pink-500 rounded-full flex items-center justify-center shadow-lg">
-              <img src={logo} alt="Logo" className="w-4 h-4 sm:w-6 sm:h-6 fill-current text-white"/>
+              <Plane className="w-4 h-4 sm:w-6 sm:h-6 fill-current text-white" />
             </div>
           </div>
-          <span className="text-lg sm:text-xl font-semibold text-gray-900">Lotusmile</span>
         </div>
 
         <div className="flex items-center space-x-2 sm:space-x-4">
@@ -60,7 +58,7 @@ export default function AppBar() {
                       />
                       <AvatarFallback
                         className="bg-gradient-to-br from-purple-600 to-pink-500 text-white text-xs sm:text-sm">
-                        USER
+                        {user?.name?.split(" ").filter(Boolean).map(w => w[0].toUpperCase()).join("")  || "ME"}
                       </AvatarFallback>
                     </Avatar>
                     <span className="hidden sm:inline text-sm sm:text-base">{user?.name}</span>
